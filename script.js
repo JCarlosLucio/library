1c3a441f680c9b733765785cf11de0e5165aa0ab
jcLucio <c.lucio3@gmail.com>
2020-04-12 18:47:48 -0500

    Add render() to show library books on shelf

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
+const shelf = document.querySelector('#shelf');
 
-let library = [];
+// Starter Books
+const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+const harryPotter = new Book('Harry Potter and the Philosopher\'s Stone', 'J.K. Rowling', 246, true);
 
+let library = [hobbit, harryPotter];
+
+// book constructor function
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
+
+// render books, loops through library and shows the books on shelf
+function render(){
+    let shelfString = ''
+    for (let i = 0; i < library.length; i++) {
+        shelfString += `
+        <div class="cover">
+            <h2>${library[i].title}</h2>
+            <h3>${library[i].author}</h3>
+            <h4>${library[i].pages}</h4>
+            <h4>${library[i].read ? 'Read' : 'Not read'}</h4>
+        </div>
+        `;
+    }
+    shelf.innerHTML = shelfString;
+}
+render();
